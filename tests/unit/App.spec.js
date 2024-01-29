2f9345dbc7de6683004925a47fd73d96171dfae5
devR2001 <144663791+devR2001@users.noreply.github.com>
2024-01-29 18:36:33 +0100

    Die anfänglichen Daten der App-Component testen

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,6 +1,13 @@
 import { expect } from "chai";
+import App from "@/App.vue";
 describe("App.vue", () => {
-  it("Dieser Test sollte erfolgreich ausgeführt werden.", () => {
+  /*  it("Dieser Test sollte erfolgreich ausgeführt werden.", () => {
     expect("Hello" + " World!").to.equal("Hello World!");
+  }); */
+  it("setzt die initialen Daten korrekt.", () => {
+    // Erwartungen mit chai formulieren
+    const initialData = App.data();
+    expect(initialData.item).to.equal("");
+    expect(initialData.items).to.deep.equal([]);
   });
 });
