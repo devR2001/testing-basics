270541241a7c7e48770b983d12338eb62a81051a
devR2001 <144663791+devR2001@users.noreply.github.com>
2024-01-29 18:32:08 +0100

    Einen ersten Test testen

---
+++ tests/unit/App.spec.js
@@ -0,0 +1,6 @@
+import { expect } from "chai";
+describe("App.vue", () => {
+  it("Dieser Test sollte erfolgreich ausgeführt werden.", () => {
+    expect("Hello" + " World!").to.equal("Hello World!");
+  });
+});
